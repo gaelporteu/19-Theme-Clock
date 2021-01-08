36dd964f376ccad9d666b5790aec80acffa4a5fd
Gael Porteu de La Morandière <gael.porteu@gmail.com>
2021-01-08 13:35:16 -0500

    Added final js and css

--- script.js
+++ script.js
@@ -24,12 +24,27 @@ function setTime() {
     const time = new Date();
     const month = time.getMonth();
     const day = time.getDay();
+    const date = time.getDate();
     const hours = time.getHours();
     const hoursForClock = hours % 12;
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+   
+    hourElement.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+    minuteElement.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
+    secondElement.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
 
-    hourElement.style.transform = `translate(-50%, -100%) rotate()`;
+    timeElement.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
+    dateElement.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
 }
 
-setTime()
+// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers/23202637#23202637
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
+
+
+setTime();
+
+setInterval(setTime, 1000);
